1a1c24400fa70d079ef752ea6a21c37edd28ab5d
Hager Fathi <76221697+gogioz@users.noreply.github.com>
2025-04-18 01:41:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const app = express();
 
 // return the body in json format
 app.use(express.json());
-app.use(express.json({ limit: "25mb" }));
-app.use(express.urlencoded({ limit: "25mb", extended: true }));
+//app.use(express.json({ limit: "25mb" }));
+//app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
 
 app.use(
